396916e47a6daf94d806a8e46fdc4ff076dabf7e
Emil Johansen <emil.johansen@gmail.com>
2017-07-21 02:35:16 +0200

    bcrypt

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define model
 const userSchema = new Schema({
@@ -11,6 +12,26 @@ const userSchema = new Schema({
   password: {
     type: String
   }
+});
+
+// On Save Hook, encrypt password
+// Invoke before saving model
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  // Generate a salt
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    // hash (encrypt) password using generated salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }
+
+      user.password = hash;
+      // Save model
+      next();
+    });
+  });
 });
 
 // Register the model
