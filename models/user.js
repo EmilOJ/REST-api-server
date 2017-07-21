226c868d0307538256055d76e40e0f3e74ae9fb1
Emil Johansen <emil.johansen@gmail.com>
2017-07-21 03:43:28 +0200

    Signin

--- models/user.js
+++ models/user.js
@@ -34,6 +34,14 @@ userSchema.pre('save', function(next) {
   });
 });
 
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) { return callback(err); }
+
+    callback(null, isMatch);
+  });
+}
+
 // Register the model
 const model = mongoose.model('user', userSchema);
 
--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,27 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 
+// Create local stratetgy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  // Verify username and password
+  User.findOne({ email: email }, function (err, user) {
+    if (err) { return done(err) ; }
+    if (!user) { return done(null, false); }
+
+    // Compare passwords
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+      if (!isMatch) { return done(null, false); }
+
+      return done(null, user);
+    });
+  });
+});
+
+// Create JWT strategy
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: config.secret
@@ -12,13 +32,11 @@ const jwtOptions = {
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
   User.findById(payload.sub, function(err, user) {
     if (err) { return done(err, false); }
+    if (!user) { return done(null, false); }
 
-    if (user) {
-      done(null, user);
-    } else {
-      done(null, false);
-    }
+    done(null, user);
   });
 });
 
 passport.use(jwtLogin);
+passport.use(localLogin);
